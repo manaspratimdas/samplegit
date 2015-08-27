ec53ad0f32339c43679b1664db45843f473061a5
Manas.Das <manas.das@spluspl.com>
2015-08-27 12:34:38 +0530

    Initial deploy to github

---
+++ HelloWorld/src/hello/MyHello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class MyHello {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is the sammple project for integration with git-jenkins-maven");
+
+	}
+
+}
